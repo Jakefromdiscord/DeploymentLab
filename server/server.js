d0f37602d6ee3cc30ef65d18eec6626a04c3646c
Jacob <jacobwamsley12@gmail.com>
2022-02-09 15:25:37 -0700

    Update

--- server/server.js
+++ server/server.js
@@ -4,4 +4,14 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log('server running on 4000'))
